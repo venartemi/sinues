632ab772d25aa0cbeed8dc4fb29ae3a73644a628
venartemi <39246870+venartemi@users.noreply.github.com>
2020-01-15 20:23:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function no_dispo(){
     alert("Ista pachina no ye encara disponible");
 }
+
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
